0bc1a83b5138f74ff6f870f2fdb53cb8445ccb64
mefetran <necroktan@gmail.com>
2015-12-05 10:12:25 +0300

    Creating List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,7 +15,52 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            
+
+            Point p3 = new Point(3, 4, '@');
+            p3.Draw();
+
+            Point p4 = new Point(7, 8, '&');
+            p4.Draw();
+
+            Console.WriteLine();
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
+            List<char> symbols = new List<char>();
+            symbols.Add('!');
+            symbols.Add('@');
+            symbols.Add('#');
+            symbols.Add('$');
+            symbols.Add('%');
+
+            foreach (char sym in symbols)
+            {
+                Console.WriteLine(sym);
+            }
+
+
             Console.ReadLine();
         }
     }
